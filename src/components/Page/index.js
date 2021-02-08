4d71c8fe0da1c95ff599bed40d5d72393774a6b5
Alexey Neklesa <alex_sunut@mail.ru>
2021-02-08 22:13:09 +0100

    Added login form

---
+++ src/components/Page/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <header>
+                header
+            </header>
+        </div>
+    );
+}
+
+export default Page;
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-import styles from  './styles.m.scss';
+import styles from './styles.m.scss';
 
 const Login = () => {
     const { register, handleSubmit } = useForm();
@@ -9,27 +9,25 @@ const Login = () => {
     const onSubmit = (e) => {
         console.log(e);
     }
-    
+
     return (
-        <div>
+        <div className={`${styles.login}`}>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div class={`card ${styles.card}`}>
+                <div class={`card`}>
                     <div class={`card-body`}>
                         <h5 class="card-title">Login</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <div class="form-group">
+                            <label for="email">Email address</label>
+                            <input ref={register} name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" />
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input ref={register} name="password" type="password" class="form-control" id="password" />
+                        </div>
+
+                        <button type="submit" class="btn btn-primary mt-3">Submit</button>
                     </div>
                 </div>
-                <div class="form-group">
-                    <label for="email">Email address</label>
-                    <input ref={register} name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" />
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input ref={register} name="password" type="password" class="form-control" id="password" />
-                </div>
-
-                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     );
